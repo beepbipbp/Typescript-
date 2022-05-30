9cad0fabb27da093c1771395043f0f2279b39c2c
beepbipbp <riskyjunk@gmail.com>
2022-05-30 15:18:53 +0900

    20220530 1518

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,5 +1,6 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+
+var url = 'https://jsonplaceholder.typicode.com/users';
 
 // dom
 var username = document.querySelector('#username');
@@ -9,17 +10,39 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {String} street
+ * @property {String} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {String} name
+ * @property {String} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+
+function fetchUser() {
+    return axios.get(url);
+}
+
 function startApp() {
-  axios
-    .get(url)
-    .then(function (response) {
-      console.log(response);
-      user = response.data;
-      // TODO: 이름, 이메일, 주소 표시하기
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    fetchUser()
+        .then(function (response) {
+            console.log(response);
+            user = response.data;
+            username.innerText = user[0].name;
+            email.innerText = user[0].email;
+            address.innerText = user[0].address.street;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 startApp();
