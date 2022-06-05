8c9c77bd1ea43a713ff035aa567c576d73526470
beepbipbp <riskyjunk@gmail.com>
2022-06-06 00:12:07 +0900

    20220606 0012

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,7 @@
+const hello: string = "hello";
+const num: number = 10;
+
+const arr1: Array<number> = [1, 2, 3];
+const arr2: number[] = [4, 5, 6];
+const heroes1: Array<string> = ["cap", "ironman", "hulk"];
+const heroes2: string[] = ["antman", "hawkeye", "blackwidow"];
