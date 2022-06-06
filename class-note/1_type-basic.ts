12a5c735bc723e6b84b033bf2adf7d135ddc7cc5
beepbipbp <riskyjunk@gmail.com>
2022-06-07 01:43:33 +0900

    20220607 0143

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -1,7 +1,25 @@
+// TS 문자열
 const hello: string = "hello";
+
+// TS 숫자
 const num: number = 10;
 
+// TS 배열
 const arr1: Array<number> = [1, 2, 3];
 const arr2: number[] = [4, 5, 6];
 const heroes1: Array<string> = ["cap", "ironman", "hulk"];
 const heroes2: string[] = ["antman", "hawkeye", "blackwidow"];
+
+// TS 튜플
+const testTuple: [string, number] = ["AAA", 100];
+
+// TS 객체
+const obj1: object = {
+    name: "AAA", age: 50
+};
+const obj2: { name: string, age: number } = {
+    name: "BBB", age: 30
+};
+
+// TS 진위값
+const option: boolean = true;
