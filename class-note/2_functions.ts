0f4fe64146ad2978a6d3d02584c9d14e8abc070f
beepbipbp <riskyjunk@gmail.com>
2022-06-08 03:04:05 +0900

    20220608 0304

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -1,3 +1,11 @@
+// 함수의 인자와 반환값에 타입을 지정
 function sum(a: number, b: number): number {
     return a + b;
 }
+
+// 옵셔널 파라미터 지정
+function log(a: string, b?: string){
+
+}
+log("aaa"); // b 파라미터를 지정하지 않아도 된다
+log("aaa", "bbb");
